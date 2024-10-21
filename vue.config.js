c0904bcf191a8091abc276262a68ce362db2b68c
Kittipong001 <xnokewro@gmail.com>
2024-10-21 17:04:39 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: "/MusicVue",
-})
-
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
